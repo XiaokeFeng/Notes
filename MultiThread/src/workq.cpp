ebf685474e17111a1504f1fa028d1cfe341aac1f
xiaokefeng <fengxiaoke.fxk@gmail.com>
2016-07-06 11:05:23 +0800

    add 注释

--- MultiThread/src/workq.cpp
+++ MultiThread/src/workq.cpp
@@ -7,6 +7,7 @@ int workq_init(workq_t* wq, int threads, void (*engine)(void* arg))
     status = pthread_attr_init(&wq->attr);
     assert(status == 0);
 
+    // work的线程都不需要调用pthread_join
     status = pthread_attr_setdetachstate(&wq->attr, PTHREAD_CREATE_DETACHED);
     if (status != 0)
     {
@@ -71,6 +72,7 @@ int workq_destroy(workq_t* wq)
 
         while (wq->counter > 0)
         {
+            // wait the other threads quit & wake up to destroy
             status = pthread_cond_wait(&wq->cv, &wq->mutex);
             if (status != 0)
             {
@@ -115,6 +117,7 @@ int workq_add(workq_t* wq, void* element)
         return status;
     }
 
+    // insert the element into queue
     if (wq->first == NULL)
         wq->first = item;
     else
@@ -124,6 +127,7 @@ int workq_add(workq_t* wq, void* element)
 
     if (wq->idle > 0)
     {
+        // wake up one of the idle threds, randomly
         status = pthread_cond_signal(&wq->cv);
         if (status != 0)
         {
@@ -133,6 +137,8 @@ int workq_add(workq_t* wq, void* element)
     }
     else if (wq->counter < wq->parallelism)
     {
+        // if no idle thread and not reach the maximum thread number,
+        // create new thread to handle the request.
         printf("creating new worker\n");
         status = pthread_create(&id, &wq->attr, workq_server, (void*)wq);
         if (status != 0)
@@ -148,6 +154,9 @@ int workq_add(workq_t* wq, void* element)
     return 0;
 }
 
+/*
+ * the routine thread for workq_t
+ */
 static void* workq_server(void* arg)
 {
     struct timespec timeout;
@@ -156,7 +165,7 @@ static void* workq_server(void* arg)
     int status;
     int timedout;
 
-    printf("a worker is starting\n");
+    printf("a worker %u is starting\n", pthread_self());
     /*
      * workq_add中已经先加锁了，这里再lock的时候，会等到workq_add的create完毕才会返回成功
      */
@@ -164,48 +173,55 @@ static void* workq_server(void* arg)
     if (status != 0)
         return NULL;
 
+    // 理论上，工作线程不退出
     while (1)
     {
         timedout = 0;
-        printf("worker waiting for work\n");
+        printf("worker %u waiting for work\n", pthread_self());
         clock_gettime(CLOCK_REALTIME, &timeout);
         timeout.tv_sec += 2;
 
         while (wq->first == NULL && !wq->quit)
         {
+            // wait the signal from the workq_add with timedout
             status = pthread_cond_timedwait(&wq->cv, &wq->mutex, &timeout);
             if (status == ETIMEDOUT)
             {
-                printf("worker wait timed out\n");
+                printf("worker %u wait timed out\n", pthread_self());
                 timedout = 1;
                 break;
             }
             else if (status != 0)
             {
                 // couldn't reach here
-                printf("worker wait failed\n");
+                printf("Never could print me. worker %u wait failed\n", pthread_self());
                 wq->counter--;
                 pthread_mutex_unlock(&wq->mutex);
                 return NULL;
             }
         }
 
-        printf("work queue: 0x%p, quit: %d\n", wq->first, wq->quit);
+        printf("work %u queue: 0x%p, quit: %d\n", pthread_self(), wq->first, wq->quit);
         we = wq->first;
 
         if (we != NULL)
         {
+            // has requests
+            // get request from queue.
             wq->first = we->next;
             if (wq->last == we)
                 wq->last = NULL;
             status = pthread_mutex_unlock(&wq->mutex);
             if (status != 0)
                 return NULL;
-
-            printf("worker calling engine\n");
+            
+            // within calling engine to handle
+            // othread worker thread can handle request from queue
+            printf("worker %u calling engine\n", pthread_self());
             wq->engine(we->data);
             free(we);
-            
+           
+            // lock : reset the state
             status = pthread_mutex_lock(&wq->mutex);
             if (status != 0)
                 return NULL;
@@ -213,7 +229,8 @@ static void* workq_server(void* arg)
 
         if (we == NULL && wq->quit)
         {
-            printf("worker shutting down\n");
+            // no requests && need quit
+            printf("xxxxxx worker %u shutting down\n", pthread_self());
             wq->counter--;
 
             if (wq->counter == 0)
@@ -225,13 +242,18 @@ static void* workq_server(void* arg)
 
         if (we == NULL && timedout)
         {
-            printf("engine terminating due to timedout\n");
+            // no request && timedout
+            
+            // if you use workq to a server,
+            // in this case, the wroker thread shouldn't quit, instead of keeping wait with timedout
+            printf("worker %u terminating due to timedout\n", pthread_self());
             wq->counter--;
+            // no quit flag, so there is no broadcast to wake up workq_destroy.
             break;
         }
     }
 
     pthread_mutex_unlock(&wq->mutex);
-    printf("working exiting\n");
+    printf("work %u exiting\n", pthread_self());
     return NULL;
 }
